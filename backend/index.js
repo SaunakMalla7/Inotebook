bbae1eb6160109c6162f2c475821c6267dfdc23c
SaunakMalla7 <95901039+SaunakMalla7@users.noreply.github.com>
2022-12-08 12:09:02 +0545

    create router for auth and notes

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 
 // Available Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+// router.use('/api/notes', require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
